745b5cefff847e01b905b4fddb8b9e1ae6cdb9d4
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-02-09 15:06:54 +0530

    refactor example

--- App.js
+++ App.js
@@ -1,137 +1,35 @@
 import {
   Page,
-  Button,
-  ButtonContainer,
   Form,
+  Button,
+  Heading,
   FormLabel,
   FormValue,
-  Heading,
+  ButtonContainer,
 } from './components';
+import useAuth from './auth';
+import React, {useMemo} from 'react';
 import {
-  authorize,
-  refresh,
-  revoke,
-  prefetchConfiguration,
-} from 'react-native-app-auth';
-import {Alert} from 'react-native';
-import React, {useState, useCallback, useMemo} from 'react';
-
-const configs = {
-  identityserver: {
-    issuer: 'https://demo.duendesoftware.com',
-    clientId: 'interactive.public',
-    redirectUrl: 'io.identityserver.demo:/oauthredirect',
-    additionalParameters: {},
-    scopes: ['openid', 'profile', 'email', 'offline_access'],
-
-    // serviceConfiguration: {
-    //   authorizationEndpoint: 'https://demo.duendesoftware.com/connect/authorize',
-    //   tokenEndpoint: 'https://demo.duendesoftware.com/connect/token',
-    //   revocationEndpoint: 'https://demo.duendesoftware.com/connect/revoke'
-    // }
-  },
-  auth0: {
-    // From https://openidconnect.net/
-    issuer: 'https://samples.auth0.com',
-    clientId: 'kbyuFDidLLm280LIwVFiazOqjO3ty8KH',
-    redirectUrl: 'https://openidconnect.net/callback',
-    additionalParameters: {},
-    scopes: ['openid', 'profile', 'email', 'phone', 'address'],
-
-    // serviceConfiguration: {
-    //   authorizationEndpoint: 'https://samples.auth0.com/authorize',
-    //   tokenEndpoint: 'https://samples.auth0.com/oauth/token',
-    //   revocationEndpoint: 'https://samples.auth0.com/oauth/revoke'
-    // }
-  },
-};
-
-const defaultAuthState = {
-  hasLoggedInOnce: false,
-  provider: '',
-  accessToken: '',
-  accessTokenExpirationDate: '',
-  refreshToken: '',
-};
+  OAUTH_ISSUER,
+  OUATH_SCOPES,
+  OAUTH_CLIENT_ID,
+  OAUTH_CALLBACK_URL,
+} from '@env';
 
 const App = () => {
-  const [authState, setAuthState] = useState(defaultAuthState);
+  const config = {
+    issuer: OAUTH_ISSUER,
+    clientId: OAUTH_CLIENT_ID,
+    redirectUrl: OAUTH_CALLBACK_URL,
+    additionalParameters: {},
+    scopes: OUATH_SCOPES.split(','),
+  };
 
-  React.useEffect(() => {
-    prefetchConfiguration({
-      warmAndPrefetchChrome: true,
-      connectionTimeoutSeconds: 5,
-      ...configs.auth0,
-    });
-  }, []);
-
-  const handleAuthorize = useCallback(
-    async provider => {
-      try {
-        const config = configs[provider];
-        const newAuthState = await authorize({
-          ...config,
-          connectionTimeoutSeconds: 5,
-          iosPrefersEphemeralSession: true,
-        });
-
-        setAuthState({
-          hasLoggedInOnce: true,
-          provider: provider,
-          ...newAuthState,
-        });
-      } catch (error) {
-        Alert.alert('Failed to log in', error.message);
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [authState],
-  );
-
-  const handleRefresh = useCallback(async () => {
-    try {
-      const config = configs[authState.provider];
-      const newAuthState = await refresh(config, {
-        refreshToken: authState.refreshToken,
-      });
-
-      setAuthState(current => ({
-        ...current,
-        ...newAuthState,
-        refreshToken: newAuthState.refreshToken || current.refreshToken,
-      }));
-    } catch (error) {
-      Alert.alert('Failed to refresh token', error.message);
-    }
-  }, [authState]);
-
-  const handleRevoke = useCallback(async () => {
-    try {
-      const config = configs[authState.provider];
-      await revoke(config, {
-        tokenToRevoke: authState.accessToken,
-        sendClientId: true,
-      });
-
-      setAuthState({
-        provider: '',
-        accessToken: '',
-        accessTokenExpirationDate: '',
-        refreshToken: '',
-      });
-    } catch (error) {
-      Alert.alert('Failed to revoke token', error.message);
-    }
-  }, [authState]);
+  const {authState, handleAuthorize, handleRefresh, handleRevoke} =
+    useAuth(config);
 
   const showRevoke = useMemo(() => {
-    if (authState.accessToken) {
-      const config = configs[authState.provider];
-      if (config.issuer || config.serviceConfiguration.revocationEndpoint) {
-        return true;
-      }
-    }
-    return false;
+    return !!authState.accessToken;
   }, [authState]);
 
   return (
